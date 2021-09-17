acd5b2ae650e46c4f72daed5c5346da71c84aeb2
Vinicius Gaspar Siqueira <vgsiqueira94@gmail.com>
2021-09-17 14:43:25 -0300

    Realizando atividade resolvida do capitulo 20

---
+++ src/com/udemy/app/Atividade20.java
@@ -0,0 +1,58 @@
+package com.udemy.app;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import com.udemy.entities.Product;
+
+public class Atividade20 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			List<Product> list = new ArrayList<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+			
+//			OptionalDouble media = list.stream().mapToDouble(Product::getPrice).average();
+			double media = list.stream()
+					.map(p -> p.getPrice())
+					.reduce(0.0, (x,y) -> x + y) / list.size();
+			
+			System.out.println("Media dos precos: " + String.format("%.2f", media));
+			
+			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			
+			List<String> names = list.stream()
+					.filter(p -> p.getPrice() <= media)
+					.map(p -> p.getName())	
+					.sorted(comp.reversed()).collect(Collectors.toList());
+			names.forEach(System.out::println);
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
+		
+	}
+
+}
